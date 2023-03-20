e8b058f48e36bd1dcd482e2762b12c01833271e6
kcritesh <kcritesh2059@gmail.com>
2023-03-20 21:45:49 +0545

    Navbar & Hero : Nav Completed & Hero nearly

---
+++ sections/Hero.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+
+const Hero = () => (
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center z-10 items-center flex-col relative">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+          <div className={styles.heroDText}></div>
+          <h1 className={styles.heroHeading}>Ness</h1>
+        </motion.div>
+      </div>
+    </motion.div>
+  </section>
+);
+
+export default Hero;
